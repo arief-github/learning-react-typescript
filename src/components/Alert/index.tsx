0e9c98c550d10f076c1ed767c55c99d9daec0e87
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-11-25 08:58:31 +0700

    Merge pull request #2
    * initialize file implementing state
    
    * add state function for event handler

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
 import Props from "../../types/props"
 
-export function Alert({ type = "information", heading, children }: Props) {
+export function Alert({ type = "information", heading, children, closable, onClose }: Props) {
+    const [visible, setVisible] = useState<boolean>(true)
+
+    if(!visible) {
+        return null
+    }
+
+    const handleCloseClick = () => {
+      setVisible(false)
+      if(onClose) {
+        onClose()
+      }
+    }
+
     return (
         <>
             <div>
@@ -11,6 +25,15 @@ export function Alert({ type = "information", heading, children }: Props) {
                 </span>
                 <span>{heading}</span>
             </div>
+            {
+                closable ? (
+                    <button aria-label="Close" onClick={handleCloseClick}>
+                        <span role="img" aria-label="Close">
+                            ❌
+                        </span>
+                    </button>
+                ) : null
+            }
             <div>
                 {children}
             </div>
