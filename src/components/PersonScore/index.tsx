d1b770446ddbd6362102c39e0777dd862484c77f
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-11-28 09:13:46 +0700

    adding state for click event on PersonScore component

--- src/components/PersonScore/index.tsx
+++ src/components/PersonScore/index.tsx
@@ -1,12 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPerson } from "../../types/getPerson";
 
 export function PersonScore() {
+    const [name, setName] = useState<string | undefined>();
+    const [score, setScore] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(true);
+
     useEffect(() => {
         getPerson().then((person) => {
-            console.log(`person data: ${person.name}`)   
+            setLoading(false);
+            setName(person.name)
         })
     }, [])
 
-    return null
+    if(loading) return <div>Loading ...</div>
+
+    return (
+        <>
+            <h3>{name}, {score}</h3>
+            <button onClick={() => { setScore(prevScore => prevScore + 1)}}>Add</button>
+            <button onClick={() => { setScore((prevScore) => prevScore - 1) }}>Subtract</button>
+            <button onClick={() => { setScore(0) }} >Reset</button>
+        </>
+    )
 }
