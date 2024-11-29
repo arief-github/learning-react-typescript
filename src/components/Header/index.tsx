8b522aeeac20d9e4d9a164bdabbe25ac39cd6155
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-11-29 15:54:02 +0700

    creating link to another page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import logo from '../../assets/react.svg'
+
+export function Header() {
+    return (
+        <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
+            <img
+                src={logo}
+                alt="Logo"
+                className="inline-block h-20"
+            />
+            <h1 className="text-2xl">React Tools</h1>
+            <nav>
+                <NavLink to="products" className={({ isActive }) => `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${isActive ? "border-white" : "border-transparent"}`}>
+                    Products
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ProductsPage } from "../pages/Products";
+import { Header } from "../components/Header";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Header />,
+    },
     {
         path: 'products',
         element: <ProductsPage />,
