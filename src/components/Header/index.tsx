c3ac7a582b19b6f6c53e6d2b880f99bd6880794d
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-12-04 19:56:03 +0700

    change usage from useNavigate into Form Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,14 @@
-import { FormEvent } from "react";
-import { NavLink, useSearchParams, useNavigate } from "react-router-dom";
+import { NavLink, useSearchParams, Form } from "react-router-dom";
 import logo from '../../assets/react.svg'
 
 export function Header() {
     const [searchParams] = useSearchParams()
-    const navigate = useNavigate()
-
-    function handleSearchSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-
-        const formData = new FormData(e.currentTarget);
-        const search = formData.get('search') as string;
-        navigate(`products/?search=${search}`)
-    }
 
     return (
         <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
-            <form
+            <Form
                 className="relative text-right"
-                onSubmit={handleSearchSubmit}
+                action="/products"
             >
                 <input
                     type="search"
@@ -27,7 +17,7 @@ export function Header() {
                     defaultValue={searchParams.get('search') ?? ''}
                     className="absolute right-0 top-0 rounded py-2 px-3 text-gray-700"
                 />
-            </form>
+            </Form>
             <img
                 src={logo}
                 alt="Logo"
